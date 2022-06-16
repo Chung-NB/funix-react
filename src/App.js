beef65ea03ade43bfc022fc640555f4504457cd8
Chung-NB <nguyenbaochung9396@gmail.com>
2022-06-16 16:33:42 +0700

    configuring react

--- src/App.js
+++ src/App.js
@@ -5,43 +5,19 @@ import { Component } from 'react';
 // import css
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="app">
                 <Navbar dark color="primary">
                     <div className="container">
-                        <NavbarBrand href="/">
-                            First Navbar item
-                        </NavbarBrand>
-                        <NavbarBrand href="/">
-                            Second Navbar item
-                        </NavbarBrand>
+                        <NavbarBrand>First Item</NavbarBrand>
+                        <NavbarBrand>Second Item</NavbarBrand>
+                        <NavbarBrand>Third Item</NavbarBrand>
                     </div>
                 </Navbar>
             </div>
-        );
+        )
     }
 }
 
