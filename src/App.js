e9275b48a11d3fdb07e58205a3bb8e3b5a436c7b
Chung-NB <nguyenbaochung9396@gmail.com>
2022-06-16 16:03:13 +0700

    install neccessary npm libraries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import libs
+import { Navbar, NavbarBrand } from 'reactstrap';
+// import components
+import { Component } from 'react';
+// import css
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">
+                            First Navbar item
+                        </NavbarBrand>
+                        <NavbarBrand href="/">
+                            Second Navbar item
+                        </NavbarBrand>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
 }
 
 export default App;
